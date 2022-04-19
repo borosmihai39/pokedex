ee4882b4e31892a6e49d738dedf1026591b938a9
borosmihai39 <borosmihai39@gmail.com>
2022-04-19 20:12:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import PokemonThumbnail from "./components/PokemonThumbnail";
 function App() {
   const [allPokemon, setAllPokemons] = useState([]);
