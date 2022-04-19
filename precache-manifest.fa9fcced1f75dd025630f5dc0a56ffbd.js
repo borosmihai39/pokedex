ca252885ca2da43cd0791b853134d9ceef4b3a44
borosmihai39 <borosmihai39@gmail.com>
2022-04-19 20:10:39 +0300

    Updates

---
+++ precache-manifest.fa9fcced1f75dd025630f5dc0a56ffbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74a9c96a35e25bbee529",
+    "url": "/pokedex/static/css/main.dbc090d3.chunk.css"
+  },
+  {
+    "revision": "74a9c96a35e25bbee529",
+    "url": "/pokedex/static/js/main.74a9c96a.chunk.js"
+  },
+  {
+    "revision": "42b09dbbb5ff2f49ef4f",
+    "url": "/pokedex/static/js/1.42b09dbb.chunk.js"
+  },
+  {
+    "revision": "69bab70dd00f4429b781",
+    "url": "/pokedex/static/js/runtime~main.69bab70d.js"
+  },
+  {
+    "revision": "0a07993421274a7daca5fbea9ce7e75e",
+    "url": "/pokedex/index.html"
+  }
+];
